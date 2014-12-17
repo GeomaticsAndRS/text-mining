3c0d581d0a7594dfa115e92d3059de138283b0d6
Davide Fiocco <davide.fiocco@epfl.ch>
2014-12-17 08:53:32 +0100

    Bug in COST abstracts download fixed

--- cost_abstracts.py
+++ cost_abstracts.py
@@ -9,6 +9,9 @@ class MyHTMLParser(HTMLParser):
 	is_abstract = False
 	content = ''
 	
+	def reset_content(self):
+		self.content = ''	
+	
 	def get_content(self):
 		return self.content
 	
@@ -24,6 +27,32 @@ domain = array(["BMBS", "CMST", "ESSEM", "FA", "FPS", "ISCH", "ICT", "MPNS", "TU
 dm = array(["BM", "CM", "ES", "FA", "FP", "IS", "IC", "MP", "TU", "TD"])
 
 parser = MyHTMLParser()
+
+for i in range(size(domain)):
+	for yy in range(6, 15):
+		for an in range(1, 11):
+			
+			action = "TD" + str(yy).zfill(2) + str(an).zfill(2)
+			
+			exists = int(urllib.urlopen("http://www.cost.eu/domains_actions/" + domain[i] + "/Actions/" + action).code) < 400
+			
+			if exists:
+				
+				print "Processing " + action
+				
+				action_page = urllib2.urlopen("http://www.cost.eu/domains_actions/" + domain[i] + "/Actions/" + action)
+				content = action_page.read()
+				action_page.close()
+				
+				abstract = ''
+				
+				parser.feed(content)
+				
+				outfile = open("output/COST/" + action + ".txt", "w")
+				abstract = parser.get_content()
+				outfile.write(re.sub('    (.*?)$', '', abstract))
+				parser.reset_content()		
+				outfile.close()
 
 for i in range(size(domain)):
 	for yy in range(6, 15):
@@ -47,28 +76,7 @@ for i in range(size(domain)):
 				outfile = open("output/COST/" + action + ".txt", "w")
 				abstract = parser.get_content()
 				outfile.write(re.sub('    (.*?)$', '', abstract))
+				parser.reset_content()		
 				outfile.close()
 
-for i in range(size(domain)):
-	for yy in range(6, 15):
-		for an in range(1, 11):
-			action = "TD" + str(yy).zfill(2) + str(an).zfill(2)
-			
-			exists = int(urllib.urlopen("http://www.cost.eu/domains_actions/" + domain[i] + "/Actions/" + action).code) < 400
-			
-			if exists:
-				
-				print "Processing " + action
-				
-				action_page = urllib2.urlopen("http://www.cost.eu/domains_actions/" + domain[i] + "/Actions/" + action)
-				content = action_page.read()
-				action_page.close()
-				
-				abstract = ''
-				
-				parser.feed(content)
-				
-				outfile = open("output/COST/" + action + ".txt", "w")
-				abstract = parser.get_content()
-				outfile.write(re.sub('    (.*?)$', '', abstract))
-				outfile.close()
+
